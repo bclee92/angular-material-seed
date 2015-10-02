37f27b34041f3f5826b92b297ec7959ec0231cf8
Thevendran <david_muthu@live.com.my>
2015-10-03 06:36:51 +0800

    Added navigation bar

--- app/app.js
+++ app/app.js
@@ -11,5 +11,17 @@ define([
             controllerUrl: 'home/home'
         }))
     });
+    app.config(function($mdThemingProvider) {
+      $mdThemingProvider.theme('default')
+        .primaryPalette('pink', {
+          'default': '400',
+          'hue-1': '100',
+          'hue-2': '600',
+          'hue-3': 'A100'
+        })
+        .accentPalette('purple', {
+          'default': '200'
+        });
+    });
     return angularAMD.bootstrap(app);
 });
